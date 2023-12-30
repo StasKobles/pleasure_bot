7b615b781636b1858f382f1b09f1fd4cbeffbd43
Stas Kobles <tank.alc@gmail.com>
2023-12-30 17:22:59 +0300

    FIX: исправил многоразовый ответ на задание

--- src/controllers/textController.ts
+++ src/controllers/textController.ts
@@ -22,55 +22,58 @@ export const textController = (bot: Telegraf<MyContext>) => {
     }
   });
   bot.on("photo", async (ctx) => {
-    const userId = ctx.from?.id;
-    if (userId) {
-      if (ctx.message.caption) {
-        const userRes = await pool.query(
-          "SELECT active_task, subscription FROM users WHERE user_id = $1",
-          [userId]
-        );
-        const userRecord = userRes.rows[0];
+    if (ctx.session.activeStep === "questAnswer") {
+      const userId = ctx.from?.id;
+      if (userId) {
+        if (ctx.message.caption) {
+          const userRes = await pool.query(
+            "SELECT active_task, subscription FROM users WHERE user_id = $1",
+            [userId]
+          );
+          const userRecord = userRes.rows[0];
 
-        if (userRecord) {
-          const { active_task, subscription } = userRecord;
+          if (userRecord) {
+            const { active_task, subscription } = userRecord;
 
-          if (active_task && subscription) {
-            // Если сообщение начинается с "/", предложим отправить отчет
+            if (active_task && subscription) {
+              // Если сообщение начинается с "/", предложим отправить отчет
 
-            // Благодарность за отчет и пересылка сообщения в админский канал
-            await ctx.reply("Спасибо за отчет! Мы скоро свяжемся с вами. 🌟");
-            ctx.session.activeStep === undefined;
+              // Благодарность за отчет и пересылка сообщения в админский канал
+              await ctx.reply("Спасибо за отчет! Мы скоро свяжемся с вами. 🌟");
+              ctx.session.activeStep === undefined;
 
-            // Получение пользователя
-            const user = ctx.from;
+              // Получение пользователя
+              const user = ctx.from;
 
-            // Полная информация о сообщении пользователя
-            const userMessage = ctx.message;
+              // Полная информация о сообщении пользователя
+              const userMessage = ctx.message;
 
-            // Формирование сообщения для админского канала
-            const adminMessage = `#${user.id}
-            Задание: ${ctx.session.todayTask?.text}
-            ${userMessage.caption}`;
+              // Формирование сообщения для админского канала
+              const adminMessage = `#${user.id}
+              Задание: ${ctx.session.todayTask?.text}
+              ${userMessage.caption}`;
 
-            await pool.query(
-              "UPDATE users SET is_completed_today = TRUE, completed_tasks = array_append(completed_tasks, $1) WHERE user_id = $2",
-              [active_task, userId]
-            );
-              
-            // Пересылка сообщения в админский канал
-            await ctx.telegram.sendPhoto(
-              adminChatId,
-              userMessage.photo[0].file_id,
-              {
-                caption: adminMessage,
-              }
-            );
+              await pool.query(
+                "UPDATE users SET is_completed_today = TRUE, completed_tasks = array_append(completed_tasks, $1) WHERE user_id = $2",
+                [active_task, userId]
+              );
+
+              // Пересылка сообщения в админский канал
+              await ctx.telegram.sendPhoto(
+                adminChatId,
+                userMessage.photo[0].file_id,
+                {
+                  caption: adminMessage,
+                }
+              );
+              ctx.session.activeStep = undefined;
+            }
           }
+        } else {
+          await ctx.reply(
+            "Пожалуйста, отправьте свой отчет заново и ОБЯЗАТЕЛЬНО прикрепите описание"
+          );
         }
-      } else {
-        await ctx.reply(
-          "Пожалуйста, отправьте свой отчет заново и ОБЯЗАТЕЛЬНО прикрепите описание"
-        );
       }
     }
   });
